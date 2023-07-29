e75bba17bd356d0348ec1eb40a7c6636d4864e00
Mohamed salah <109218485+MOsalahCS@users.noreply.github.com>
2023-07-30 01:40:32 +0300

    commited
    project

---
+++ project_e-commerce/e_commerce/products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User =get_user_model()
+
+
+
+# Create your models here.
+
+
+# class Product(models.Model):
+    
+
+
