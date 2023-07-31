ef22e8a1831411d64d7ab3f116eb90ca9abff6ff
Mohamed Hossam <80326979+mhdhossam@users.noreply.github.com>
2023-07-31 03:20:54 +0300

    Update models.py

--- project_e-commerce/e_commerce/user/models.py
+++ project_e-commerce/e_commerce/user/models.py
@@ -21,6 +21,20 @@ class UserProfile(models.Model):
 
 
 
+class Useraddress(models.Model):
+    BILLING='B'
+    SHIPPING ='S'
+    ADDRESS_TYPE=[ BILLING, SHIPPING]
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    city=models.CharField(max_length=100)
+    country=CountryField()
+    street_address=models.CharField(max_length=100)
+    apartment_address=models.CharField(max_length=100)
+    postal_code=models.CharField( max_length=10)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self) :
+        return self.user
 
 
 
